f0a215663e59c5cdd26f1f06b8f02d4e4bb76bd3
Kim <kimasdilla@gmail.com>
2023-02-21 20:41:19 +0800

    update browserlist

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
 import { Box } from '@mui/material';
-import Map from '!react-map-gl';
+import Map from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-
-import mapboxgl from 'mapbox-gl'; // This is a dependency of react-map-gl even if you didn't explicitly install it
-
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const MapPage = () => {
   return (
