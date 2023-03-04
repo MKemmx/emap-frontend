7537d7b82dce0c9952737abe37cdd236261726ad
Kim <kimasdilla@gmail.com>
2023-03-04 20:47:57 +0800

    Fixing map prod

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -4,6 +4,19 @@ import { Box } from '@mui/material';
 import Map from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+// had this.
+import ReactMapGL, { FlyToInterpolator, NavigationControl } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// added the following 6 lines.
+import mapboxgl from 'mapbox-gl';
+
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 const MapPage = () => {
   return (
     <Box sx={{ width: '100%', height: '100%' }}>
